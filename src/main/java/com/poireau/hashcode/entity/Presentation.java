3887ac7864dfcebfe4eb08481a57888995fce684
b-fischer <fischer_baptiste@hotmail.fr>
2019-02-28 19:34:52 +0100

    feat: ajout presentation

---
+++ src/main/java/com/poireau/hashcode/entity/Presentation.java
@@ -0,0 +1,13 @@
+package com.poireau.hashcode.entity;
+
+import java.util.List;
+
+public class Presentation {
+
+	private List<Slide> slides;
+	
+	public Presentation() {
+		// par defaut
+	}
+	
+}
